6748edf468a4f17cdcf78bc3c9a7574d25548d0a
dung1311 <tiendung13112004@gmail.com>
2024-07-11 23:04:44 +0700

    nothing

--- src/controllers/chatbot_controllers.js
+++ src/controllers/chatbot_controllers.js
@@ -32,24 +32,31 @@ let postWebhook = (req, res) => {
     let body = req.body;
 
     console.log(`\u{1F7EA} Received webhook:`);
-    console.dir(body);
+    // console.dir(body);
     // Send a 200 OK response if this is a page webhook
 
     if (body.object === "page") {
 
-        body.entry.forEach(function (entry) {
+        body.entry.forEach(function(entry) {
+
+            // Gets the body of the webhook event
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
-
+          
+          
+            // Get the sender PSID
             let sender_psid = webhook_event.sender.id;
-            console.log('Sender psid: ' + sender_psid);
-
+            console.log('Sender PSID: ' + sender_psid);
+          
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
             if (webhook_event.message) {
-                handleMessage(sender_psid, webhook_event.message);
+              handleMessage(sender_psid, webhook_event.message);        
             } else if (webhook_event.postback) {
-                handlePostback(sender_psid, webhook_event.postback);
+              handlePostback(sender_psid, webhook_event.postback);
             }
-        });
+            
+          });
 
         // Returns a '200 OK' response to all requests
         res.status(200).send("EVENT_RECEIVED");
@@ -63,7 +70,7 @@ let postWebhook = (req, res) => {
 function handleMessage(sender_psid, received_message) {
 
     let response;
-    let sender_psid = 8047270265389746;
+    
     // Check if the message contains text
     if (received_message.text) {
 
